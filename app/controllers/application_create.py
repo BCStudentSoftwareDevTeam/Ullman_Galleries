ab389f14a0ed4c2920d4381bb9ea44221af0e074
Kenny Nelson <nelsonk@berea.edu>
2018-01-29 22:24:29 +0000

    CV and Personal Uploads are now saved into the database along with the other inputs on the form. TO-DO: Allow the upload of images from a dropzone, create thumbnails and save them into the database

--- app/controllers/application_create.py
+++ app/controllers/application_create.py
@@ -16,15 +16,20 @@ def create(gid):
 
 @app.route('/application/submit/<gid>', methods=["GET", "POST"])
 def application_submit(gid):
-    gallery = Galleries.get(Galleries.gid==gid)
+    # retrieve the specific gallery for which the application was submitted
+    gallery         = Galleries.get(Galleries.gid==gid)
+    
+    # create a Formqueries class object to insert data from the form and attachment files into the database. 
     application_obj = FormQueries()
      
-    data        = request.form
-    submit_date = time.strftime("%m%d%y")
+    # retrieve data from the form
+    data            = request.form
     
+    submit_date     = time.strftime("%m/%d/%y %H:%M:%S")
+  
     try:
         #Insert data into database
-        submission =  application_obj.insert(data['firstName'],
+        submission   =  application_obj.insert(data['firstName'],
                                              data['lastName'],
                                              data['streetAddress'],
                                              data['addressLine2'],
@@ -45,40 +50,70 @@ def application_submit(gid):
 
         
     if submission != False:
+        # save uploads in the database with the associated FID
         fid  = submission.fid
         try:
-            # save uploads in the database with the associated FID
+            
             if 'cv' in request.files:
-                cv = request.files['cv']
-                cv_ext    = (str(cv.filename.split(".").pop())).replace(" ","")
-                cv_filename = "cv_{}".format(data['firstName']+ '_'+ data['lastName'])
+                # retrieve the cv uploaded in the application form
+                cv             = request.files['cv']
+                
+                # store the file extension 
+                cv_ext         = (str(cv.filename.split(".").pop())).replace(" ","")
+                
+                # rename the file uploaded to a specific format
+                cv_filename    = "cv_{}".format(data['firstName']+ '_'+ data['lastName'])
+                
+                # store the absolute path where the file will be stored on the server
                 cv_upload_path = getAbsolutePath(cfg['paths']['files'],cv_filename,True)
+                
+                # save the file on the server
                 cv.save(cv_upload_path)
-                application_obj.insert_cv_file(fid,cv_filename,cv_upload_path,cv_ext)
+                
+                # save the file in the database in the Files table
+                application_obj.insert_attachment_file("cv", fid,cv_filename,cv_upload_path,cv_ext)
+        
         except Exception as e:
             print (e)
+        
             flash("An error occured while saving cv file!")
+        
             return render_template('views/application_create.html',gid=gid, gallery = gallery, cfg=cfg)
         
         try:    
+        
             if 'statement' in request.files:   
-                statement = request.files['statement']
-                statement_ext    = (str(statement.filename.split(".").pop())).replace(" ","")
-                statement_filename = "personal_statement_{}".format(data['firstName']+ '_'+ data['lastName'])
+                # retrieve the personal statement uploaded in the application form 
+                statement             = request.files['statement']
+                
+                # store the file extension 
+                statement_ext         = (str(statement.filename.split(".").pop())).replace(" ","")
+                
+                # rename the file uploaded to a specific format
+                statement_filename    = "personal_statement_{}".format(data['firstName']+ '_'+ data['lastName'])
+                
+                # store the absolute path where the file will be stored on the server
                 statement_upload_path = getAbsolutePath(cfg['paths']['files'],statement_filename,True)
+                
+                # save the file on the server
                 statement.save(statement_upload_path)
-                application_obj.insert_statement_file(fid,statement_filename,statement_upload_path,statement_ext)
+                
+                # save the file in the database in the Files table
+                application_obj.insert_attachment_file("statement", fid,statement_filename,statement_upload_path,statement_ext)
+        
         except Exception as e:
             print (e)
-            flash("An error occured while saving statement")
+        
+            flash("An error occured while saving the personal statement file!")
+        
             return render_template('views/application_create.html',gid=gid, gallery = gallery, cfg=cfg)
             
         flash("Your application was successfully submitted.")
+ 
         return render_template('views/application_review.html',gid=gid, gallery = gallery, cfg=cfg)
-
-    
 
     else:
         flash("Your application was not submitted, for an error occured in the process.")
+ 
     return render_template('views/application_create.html',gid=gid, gallery = gallery, cfg=cfg)
-
+ 
