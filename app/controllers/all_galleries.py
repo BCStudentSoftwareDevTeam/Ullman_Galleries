c96167cb03916abc62634addb53c2b67d1728011
Jonathan <chauwaj@berea.edu>
2018-01-31 06:57:33 +0000

    Redo: Modified queries,Added method for status in Gallery model and fixed minor errors in html

--- app/controllers/all_galleries.py
+++ app/controllers/all_galleries.py
@@ -1,4 +1,4 @@
-from app.allImports import * 
+from app.allImports import *
 from app.logic.validation import *
 from werkzeug.security import check_password_hash
 from flask import session
@@ -10,71 +10,18 @@ from datetime import datetime
 
 @app.route('/', methods=["GET","POST"])
 def all_galleries():
+    gallerySubmissionQuerry = Galleries().select().join(Forms, JOIN_LEFT_OUTER).annotate(Forms) #gets all gallery submissions
+    galleryTosubmissions ={}  #maps each gallery to the number of submissions it has e.g { Gallery-1 : 10-submissions }
+    
   
-    formGalleryIds=[] 
-    gallaryIds = []
     
-    
-    map1={} #maps gallery ids and titles
-    map2={} #maps gallary to the total numbe of submissions it has
-    map3 = {} #maps gallery to its status e.g closed,open,active
-    map4 = {} #Stores final dates for each gallery depending on its status.
-    
-    
-    
-    
-    
-    for gallery in Galleries.select():
-        map1[gallery.gid]=gallery.title
-        gallaryIds.append(gallery.gid)
-        
-    for form in Forms.select():
-        formGalleryIds.append(form.gallery_id)
-        
-    
-    #initializing gallery submission count
-    for gallerycount in gallaryIds:
-        map2[gallerycount] = 0
-        
-    
-    #Counting number of submissions in each gallery
-    for galleryid in formGalleryIds:
-        if galleryid in map2:
-            map2[galleryid] +=1
-            
-    
-    today = datetime.now() #For purposes of determining the status of a galler
-    for gallery_name in map2:
-        #getting dates for each gallery
-        obj=Galleries.get(gid=gallery_name)
-        opened= obj.open_date
-        closed=obj.close_date
-        
-        
-        if today >= opened and today >= closed:
-            #if closed deadline passed,status=closed
-            map3[gallery_name]="Closed :"
-            close_date=str(closed)
-            map4[gallery_name]=close_date
-        if today <= opened and today <= closed:
-            #if both open and closed deadlines have not passed, we lable it as "coming soon"
-            map3[gallery_name]="Coming Soon :"
-            open_date=str(opened)
-            map4[gallery_name]=open_date
-            
-        if today >= opened and today <= closed:
-            #if gallery opened but has not closed, we label it as active
-            map3[gallery_name]="Active :"
-            active_date=str(opened)
-            active_closed_date=str(closed)
-            active_period = active_date + " - "  + active_closed_date
-            map4[gallery_name]=active_period
-            
-   
+    for gallery in gallerySubmissionQuerry:
+        galleryStatus = gallery.GalleryStatus()  #gets gallery statuses 
+        galleryTosubmissions[str(gallery)]=gallery.count 
+
     return render_template('views/all_galleries.html',
-                                                map1=map1,
-                                                map2=map2,
-                                                map3=map3,
-                                                map4=map4)
+                                                galleryTosubmissions=galleryTosubmissions,
+                                                galleryStatus=galleryStatus,
+                                                )
 
 
--- app/models/Galleries.py
+++ app/models/Galleries.py
@@ -1,5 +1,6 @@
 from app.models.util import *
 from app.models.Files import *
+from datetime import datetime
 
 class Galleries (baseModel):
   gid           = IntegerField(primary_key=True)
@@ -11,4 +12,25 @@ class Galleries (baseModel):
 
   def __str__(self):
     return self.title
+    
+    
+  def GalleryStatus(self):
+    '''Determines the status for each gallery instance'''
+    status ={}
+    galleries=Galleries.select()
+    today = datetime.now()
+    for gallery in galleries:
+      openDate = gallery.open_date
+      closeDate = gallery.close_date
+      if today >= openDate and today >= closeDate: #if gallery closed,status = closed
+        status[str(gallery)]="Closed" + " " + str(closeDate)
+      if today <= openDate and today <= closeDate: #if gallery not open but coming soon, status = coming soon
+        status[str(gallery)] = "Coming Soon " +" " + str(openDate)
+      if today >= openDate and today <= closeDate: #if gallery open, status = active
+        status[str(gallery)]= "Active " + " " + str(openDate) + " " + "to" + " " + str(closeDate)
+          
+    return status
+
+
+    
 
