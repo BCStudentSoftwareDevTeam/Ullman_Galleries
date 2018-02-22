31a650305c7a94a41dac65fdbd568492d7927b39
Jesson Soto Ventura <jesson@sotoventura.com>
2018-02-22 15:00:51 -0500

    Cleaned Dummy Data

--- app/models/Galleries.py
+++ app/models/Galleries.py
@@ -4,11 +4,11 @@ from app.models.Files import *
 class Galleries (baseModel):
   gid             = PrimaryKeyField()
   title           = TextField()
-  open_date       = DateTimeField()
-  close_date      = DateTimeField()
-  description     = TextField()
-  banner          = ForeignKeyField(Files)
-  folder_name     = TextField()
+  open_date       = DateTimeField(null=True)
+  close_date      = DateTimeField(null=True)
+  description     = TextField(null=True)
+  banner          = ForeignKeyField(Files, null=True)
+  folder_name     = TextField(null=True)
 
   def __str__(self):
     return self.title
